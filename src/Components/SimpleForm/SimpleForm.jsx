bd2851a7d7337f103507399cd86e57fb9f3673bd
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-09-23 02:16:39 +0600

    Handle form with onSubmit and access field data

---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,24 @@
+const SimpleForm = () => {
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.phone.value)
+        console.log("Form submited");
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br></br>
+                <input type="email" name="email" />
+                <br></br>
+                <input type="text" name="phone" />
+                <br></br>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
